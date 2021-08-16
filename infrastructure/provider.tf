48d210661c021d5187657ca73cc747c8e5094296
alan_gularte@dell.com <alan_gularte@dell.com>
2021-08-15 23:23:49 -0300

    Organize the terraform files and definition of backend tf s3

---
+++ infrastructure/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Centralizar o arquivo de controle de estado do terraform
+# O S3 bucket já tem que estar previamente criado (manualmente)
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-alangularte"
+    key = "state/terraform.tfstate"
+    region = "us-esat-2"
+  }
+}
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -19,16 +19,3 @@ resource "aws_s3_bucket" "datalake" {
     CURSO = "EDC"
   }
 }
-
-#Cria o arquivo emr-code/pyspark/job_spark_from_tf.py no S3 utilizando o arquivo job_spark.py como origem
-resource "aws_s3_bucket_object" "codigo_spark" {
-  bucket = aws_s3_bucket.datalake.id
-  key    = "emr-code/pyspark/job_spark_from_tf.py"
-  acl    = "private"
-  source = "../job_spark.py"
-  etag   = filemd5("../job_spark.py")
-}
-
-provider "aws" {
-  region = var.aws_region
-}
