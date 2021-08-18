13209997c1de536ec28deef0b118eaebd400e170
alan_gularte@dell.com <alan_gularte@dell.com>
2021-08-18 20:56:15 -0300

    Add aws_iam_role for lambda

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,3 +1,28 @@
+resource "aws_iam_role" "lambda" {
+  name = "LambdaRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": "AssumeRole"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+}
 resource "aws_iam_policy" "lambda" {
   name        = "AWSLambdaBasicExecutionRolePolicy"
   path        = "/"
